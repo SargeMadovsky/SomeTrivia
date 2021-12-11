5222807e30c954b63ccddfe4c55bd96088441408
Sarge Madovsky <madovsky@gmail.com>
2021-12-11 22:38:55 +0300

    Initial commit

---
+++ Assets/scripts/Question.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Question {
+
+	private string question;
+	private string correctAnswer;
+	private string[] dummyAnswers = new string[1];
+
+	public Question(string question, string correctAnswer, string firstDummyAnswer)
+	{
+		this.question = question;
+		this.correctAnswer = correctAnswer;
+		dummyAnswers[0] = firstDummyAnswer;
+	}
+
+	public string getQuestion()
+	{
+		return question;
+	}
+
+	public string getCorrectAnswer()
+	{
+		return correctAnswer;
+	}
+
+	public string getDummyAnswer(int index)
+	{
+		return dummyAnswers[index];
+	}
+}
